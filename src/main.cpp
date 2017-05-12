484151a1d298464ab670f7ea71d476966de2e742
folker85 <folker85@gmail.com>
2017-05-12 23:20:40 +0300

    Drawing a humble triangle.

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,33 @@ int main()
 	}
 
 	setupViewport(window);
+
+	const std::string vertex_shader_source_code_path = "shaders\\Simple.vertex";
+	const std::string fragment_shader_source_code_path = "shaders\\Simple.fragment";
+	ShaderProgram program(vertex_shader_source_code_path, fragment_shader_source_code_path);
+
+	// Set up vertex data (and buffer(s)) and attribute pointers
+	GLfloat vertices[] = {
+		// Positions        
+		0.5f, -0.5f, 0.0f,  // Bottom Right
+		-0.5f, -0.5f, 0.0f,  // Bottom Left
+		0.0f,  0.5f, 0.0f   // Top 
+	};
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0); // Unbind VAO
+
 	
 	while (!glfwWindowShouldClose(window))
 	{
@@ -47,6 +74,11 @@ int main()
 		// Render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		program.use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 	}
